80302d268b6186c2755cf82631b34bca566c19f3
soapmactavish23 <ricknogueira1231@gmail.com>
2026-08-15 17:30:01 -0300

    2.14. Ajustando CORS

--- src/main/java/com/algaworks/algashop/gateway/ecommerce/GatewayEcommerceSecurityConfig.java
+++ src/main/java/com/algaworks/algashop/gateway/ecommerce/GatewayEcommerceSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algashop.gateway.ecommerce;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -11,9 +12,11 @@ public class GatewayEcommerceSecurityConfig {
 
     @Bean
     SecurityWebFilterChain defaSecurityWebFilterChain(ServerHttpSecurity http) {
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+        http.cors(Customizer.withDefaults())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorize -> authorize
                         .pathMatchers("/actuator/health").permitAll()
+                        .pathMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
                         .pathMatchers("/api/**").authenticated()
                         .anyExchange().denyAll()
                 )
